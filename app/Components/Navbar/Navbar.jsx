1902c1435fe283b2954e4ce3527cd7debe78eb02
Ronell <rvillamil_180000000794@uic.edu.ph>
2023-11-06 15:59:38 +0800

    Initial Commit

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -11,30 +11,28 @@ function NavBar() {
     setToggle(!toggle);
   };
   return (
-    <div className="sticky top-0">
-      <nav className=" flex items-center justify-between w-full min-h-[16] shadow-md p-4">
-        <div className=" border-4 animate-bounce">
+    <div className="sticky top-0 ">
+      <nav className=" flex items-center justify-between w-full min-h-[16] shadow-md p-4 ">
+        <div className=" text-2xl animate-bounce font-mono">
           <Link href="/">RCV</Link>
         </div>
-        <div className="border-4 hidden md:block ">
+        <div className=" hidden md:block ">
           <ul className="flex">
             {NavBarLinks.map((item) => (
               <li
                 key={item.id}
-                className="w-[10rem] flex justify-around text-md font-mono ">
+                className="w-[10rem] flex justify-around text-lg font-serif ">
                 <Link href={item.path}>{item.title}</Link>
               </li>
             ))}
           </ul>
         </div>
-        <div
-          className="border-4 cursor-pointer z-20 md:hidden"
-          onClick={onClick}>
+        <div className=" cursor-pointer z-20 md:hidden" onClick={onClick}>
           {!toggle ? <CiMenuFries size={30} /> : <AiOutlineClose size={30} />}
         </div>
       </nav>
       <div
-        className={`bg-red-600 w-full fixed top-0 text-xl z-[999999999]" font-mono ${
+        className={`bg-bg w-full fixed top-0 text-xl z-[999999999]" font-mono ${
           toggle
             ? " translate-y-100 ease-in duration-300"
             : "-translate-y-full ease-in-out duration-300"
